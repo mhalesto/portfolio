85eaed1d8912b869e955381bd8302889ac920e0a
mhalesto <mbanjwa.hg@gmail.com>
2023-01-07 17:12:37 +0200

    fix: project config, add router & pages

---
+++ src/pages/contact/index.js
@@ -0,0 +1,11 @@
+import Layout from "../../components/layout";
+
+const Contact = () => {
+  return (
+    <Layout>
+      <h1>Contact component</h1>
+    </Layout>
+  )
+}
+
+export default Contact;
